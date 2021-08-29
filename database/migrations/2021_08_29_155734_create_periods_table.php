f1548ac119a38f8c2617d9a61026355f9329a993
ibnnajjaar <ibnahnajjaar@gmail.com>
2021-08-29 21:02:10 +0500

    - added migrations

---
+++ database/migrations/2021_08_29_155734_create_periods_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeriodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('periods', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('subject_id')->constrained()->cascadeOnDelete();
+            $table->dateTime('start_at');
+            $table->dateTime('end_at');
+            $table->string('weekday');
+            $table->text('details')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('periods');
+    }
+}
---
+++ database/migrations/2021_08_29_155859_create_assessments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssessmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assessments', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->dateTime('assessment_at')->nullable();
+            $table->foreignId('subject_id')->constrained()->cascadeOnDelete();
+            $table->text('description')->nullable();
+            $table->unsignedInteger('total_marks')->nullable();
+            $table->unsignedInteger('weight')->nullable();
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assessments');
+    }
+}
