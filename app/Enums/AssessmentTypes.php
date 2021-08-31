2300353d2c300ed8163c6bd4e2c82c5b12e6310d
ibnnajjaar <ibnahnajjaar@gmail.com>
2021-08-31 16:14:49 +0500

    - adding assessment models n request

---
+++ app/Enums/AssessmentTypes.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enums;
+
+use Spatie\Enum\Enum;
+
+/**
+ * @method static self exam()
+ * @method static self assignment()
+ * @method static self tutorial()
+ */
+class AssessmentTypes extends Enum
+{
+}
