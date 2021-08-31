c4358fdf8cfc276933bf6dfa67a048d74864467b
ibnnajjaar <ibnahnajjaar@gmail.com>
2021-08-31 16:57:34 +0500

    - thats it for this tutorial, have to go somewhere :D

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,8 +7,10 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+            <div class="flex flex-row w-full space-x-4">
+                <div class="w-1/3 bg-white shadow-xl rounded-lg p-8 text-2xl">{{ __('Subject :count', ['count' => 0]) }}</div>
+                <div class="w-1/3 bg-white shadow-xl rounded-lg p-8 text-2xl">{{ __('Periods :count', ['count' => 0]) }}</div>
+                <div class="w-1/3 bg-white shadow-xl rounded-lg p-8 text-2xl">{{ __('Assessments :count', ['count' => 0]) }}</div>
             </div>
         </div>
     </div>
