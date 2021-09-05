74bff22894a88bb8d7494c5cf4041a269238d549
ibnnajjaar <ibnahnajjaar@gmail.com>
2021-09-05 15:29:11 +0500

    - quick fix order

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,11 @@ class HomeController extends Controller
     public function __invoke(Request $request)
     {
         $today = strtolower(Carbon::now()->format('l'));
-        $period_groups = Period::query()->with('subject')->get()->groupBy('weekday');
+        $period_groups = Period::query()
+            ->with('subject')
+            ->orderBy('start_at')
+            ->get()
+            ->groupBy('weekday');
 
         $active_group = $period_groups->filter(function ($period_group, $key) use ($today) {
             return $today == $key;
